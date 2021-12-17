746323ce135ee6583969817766c0baf6bd88bed1
ragasiri <96305061+ragasiri@users.noreply.github.com>
2021-12-17 21:33:49 +0530

    initial commit

---
+++ virtualassistant.py
@@ -0,0 +1,69 @@
+import speech_recognition as sr
+import datetime
+import subprocess
+import pywhatkit
+import pyttsx3
+import webbrowser
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+
+engine.setProperty('voice',voices[1].id)
+
+recognizer=sr.Recognizer()
+
+def cmd():
+    with sr.Microphone() as source:
+
+        print("Clearing background noises...Please wait")
+
+        recognizer.adjust_for_ambient_noise(source,duration=0.5)
+
+        print('Ask me anything..')
+
+        recordedaudio=recognizer.listen(source)
+
+    try:
+
+        command=recognizer.recognize_google(recordedaudio,language='en_US')
+
+        command=command.lower()
+
+        print('Your message:', format(command))
+
+    except Exception as ex:
+
+        print(ex)
+
+    if 'chrome'in command:
+
+        a='Opening chrome..'
+        engine.say(a)
+        engine.runAndWait()
+        programName="chrome.exe"
+        subprocess.popen([programName])
+    if 'time'in command:
+        time=datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        engine.say(time)
+        engine.runAndWait()
+    if 'play' in command:
+        a='opening youtube..'
+        engine.say(a)
+        engine.say(a)
+        engine.runAndWait()
+        pywhatkit.playonyt(command)
+    if'youtube'in command:
+        b='opening youtube'
+        engine.say(b)
+        engine.runAndWait()
+        webbrowser.open('www.youtube.com')
+    
+while True:
+
+    cmd()
+
+
+
+
+
